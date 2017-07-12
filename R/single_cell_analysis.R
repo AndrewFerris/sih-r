a0806c78dcd3751621c8c70210b38e4231454361
AndrewFerris <ferris.andrewg@gmail.com>
2017-07-12 16:28:56 +1000

    Added some documentation for the single_cell_analysis function.

--- R/single_cell_analysis.R
+++ R/single_cell_analysis.R
@@ -1,18 +1,22 @@
-library(devtools)
-library(ggbiplot)
-library(ggfortify)
-library(gplots)
-library(pvclust)
-library(plyr)
-library(reshape)
-library(scater)
-library(edgeR)
-library(scran)
-library(d3heatmap)
-library(DESeq2)
-library(statmod)
-library(genefilter)
-library(limSolve)
+#' @name deconvolution_normalization
+#' 
+#' @title Deconvolution Normalization for Bioinformatics Pre-Processing
+#' 
+#' @description A function used for pre-processing standard bioinformatics file formats into a workable format in the R environment.
+#' 
+#' @param data A text delimited data file in standard bioinformatics format.
+#' @param target_dir The specific local target directory to write out plotting to.
+#' @param plots A logical paramater specifying whether to return plotting.
+#' 
+#' @return ave.counts The average counts from the SCEset
+#' @return numcells The number of expressions from the SCEset
+#' 
+#' @import ggbiplot ggfortify gplots pvclust plyr reshape scater edgeR scran d3heatmap DESeq2 statmod genefilter limSolve
+#' 
+#' @author Anushi Shah
+#' 
+#' @export
+NULL
 
 deconvolution_normalization <- function(data, target_dir, plots){
   
